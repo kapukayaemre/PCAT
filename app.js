467f0ec4d877ba6209c3d175ef3bc2f606b81ec8
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-02-27 03:34:16 +0300

    Static Files Uploaded

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req,res) => {
-  const photo = {
-    id : 1,
-    name : "Photo Name",
-    description : "Photo Description"
-  }
-  res.send(photo);
-})
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
+};
 
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware Log 2');
+  next();
+};
+
+//! MIDDLEWARES
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
 
 const port = 3000;
 app.listen(port, () => {
