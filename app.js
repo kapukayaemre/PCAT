eb9b2ef3fb5d580cdca8afba17b9cda09f96c743
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-02-27 04:06:51 +0300

    View pages created and partials created,integreted

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
-const myLogger = (req, res, next) => {
-  console.log('Middleware Log 1');
-  next();
-};
-
-const myLogger2 = (req, res, next) => {
-  console.log('Middleware Log 2');
-  next();
-};
+//! TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 //! MIDDLEWARES
 app.use(express.static('public'));
-app.use(myLogger);
-app.use(myLogger2);
 
+//! ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
 
 const port = 3000;
 app.listen(port, () => {
